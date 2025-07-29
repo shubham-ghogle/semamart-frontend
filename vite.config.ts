316713c090fe9f669c8e2a5ef91e32a791ed77d6
shubham.ghogle <shubham.ghogle@innovyatech.com>
2025-07-29 21:37:02 +0530

    library:shadcn-ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
   //here we are defining proxy to for our api url
   server: {
     proxy: {
