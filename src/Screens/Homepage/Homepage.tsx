415524677849d4de4359b6844f736b613564f636
Ritik Kumar Singh <ritik.kumar.singh@innovyatech.com>
2025-08-11 12:05:39 +0530

    Eror fix

--- src/Screens/Homepage/Homepage.tsx
+++ src/Screens/Homepage/Homepage.tsx
@@ -4,7 +4,7 @@ import ProductShowcase from "../../components/Homepage/ProductShowcase";
 import ImageSlider from "../../components/Homepage/ImageSlider";
 
 export default function Homepage() {
-  const { status: productFetchingStatus } = useQuery({
+  const { status: productFetchingStatus, data: products } = useQuery({
     queryKey: ["products"],
     queryFn: getProducts,
     staleTime: Infinity,
@@ -12,11 +12,11 @@ export default function Homepage() {
 
   return (
     <section>
-      <ImageSlider image="banner_Equipment.png"/>
+      <ImageSlider image="banner_Equipment.png" />
       <div className="container max-w-[1060px] mx-auto px-4 pt-12">
-        <ProductShowcase status={productFetchingStatus} title="Most Popular" />
-        <ProductShowcase status={productFetchingStatus} title="Best Sellers" />
-        <ProductShowcase status={productFetchingStatus} title="Recent Items" />
+        <ProductShowcase status={productFetchingStatus} title="Most Popular" products={products || []} />
+        <ProductShowcase status={productFetchingStatus} title="Best Sellers" products={products || []} />
+        <ProductShowcase status={productFetchingStatus} title="Recent Items" products={products || []} />
       </div>
     </section>
   );
