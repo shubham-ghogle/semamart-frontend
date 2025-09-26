f30aacc26dfd93f8e09df82a68af909bcebd1c65
shubham.ghogle <shubham.ghogle@innovyatech.com>
2025-09-26 16:23:19 +0530

    api url prod

--- src/data.ts
+++ src/data.ts
@@ -1,2 +1,3 @@
-export const API_URL = "http://localhost:8000/api/v2/"
-export const BASE_URL = "http://localhost:8000/"
+// export const API_URL = "http://localhost:8000/api/v2/"
+export const API_URL = "https://test.semamart.com/api/v2/"
+export const BASE_URL = "https://test.semamart.com/"
