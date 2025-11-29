7391a95f88274cc0d74851735b796ec06d10c11e
apoorvjauhariinfo <apoorv.jauhari@innovyatech.com>
2025-11-29 21:43:31 +0530

    Live API

--- src/data.ts
+++ src/data.ts
@@ -2,10 +2,10 @@
 // export const API_URL = "https://www.semamart.com/api/v2/"
 //export const API_URL = "http://localhost:8000/api/v2/";
 //export const BASE_URL = "http://localhost:8000/"
-// export const BASE_URL = "https://www.semamart.com/"
+ export const BASE_URL = "https://www.semamart.com/"
 //export const BASE_URL = "http://localhost:8000/";
 //  export const API_URL = "https://test.semamart.com/api/v2/"
 //  export const BASE_URL = "https://test.semamart.com/"
-// export const API_URL = "https://www.semamart.com/api/v2/"
-  export const API_URL = "https://test.semamart.com/api/v2/"
-  export const BASE_URL = "https://test.semamart.com/"
+ export const API_URL = "https://www.semamart.com/api/v2/"
+  //export const API_URL = "https://test.semamart.com/api/v2/"
+  //export const BASE_URL = "https://test.semamart.com/"
