0fdd4afdbb892b47458a38afc7777123c8993a51
shubham.ghogle <shubham.ghogle@innovyatech.com>
2026-01-15 11:43:14 +0530

    test apis

--- src/data.ts
+++ src/data.ts
@@ -4,10 +4,10 @@
 
 
 // // live apis
-export const API_URL = "https://www.semamart.com/api/v2/"
-export const BASE_URL = "https://www.semamart.com/"
+// export const API_URL = "https://www.semamart.com/api/v2/"
+// export const BASE_URL = "https://www.semamart.com/"
 
 
 // test apis
-// export const API_URL = "https://test.semamart.com/api/v2/"
-// export const BASE_URL = "https://test.semamart.com/"
+export const API_URL = "https://test.semamart.com/api/v2/"
+export const BASE_URL = "https://test.semamart.com/"
