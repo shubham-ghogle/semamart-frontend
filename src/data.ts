1c220ea9552c27970badb91fb3771f8b263b335d
apoorvjauhariinfo <apoorv.jauhari@innovyatech.com>
2026-01-29 13:03:39 +0530

    ENV VAr

--- src/data.ts
+++ src/data.ts
@@ -3,5 +3,5 @@
 // export const BASE_URL = "http://localhost:8000/"
 
 
-export const API_URL = import.meta.env.VITE_API_URL;
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const API_URL = process.env.VITE_API_URL;
+export const BASE_URL = process.env.VITE_BASE_URL;
