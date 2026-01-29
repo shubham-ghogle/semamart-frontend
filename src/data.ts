05cbfa45b1f53d7a2b470155da3d131abf2447bb
apoorvjauhariinfo <apoorv.jauhari@innovyatech.com>
2026-01-29 12:59:02 +0530

    DATA.TS - ENV

--- src/data.ts
+++ src/data.ts
@@ -1,13 +1,7 @@
-// local dev apis
-export const API_URL = "http://localhost:8000/api/v2/";
-export const BASE_URL = "http://localhost:8000/"
+// // local dev apis
+// export const API_URL = "http://localhost:8000/api/v2/";
+// export const BASE_URL = "http://localhost:8000/"
 
 
-// // live apis
-// export const API_URL = "https://www.semamart.com/api/v2/"
-// export const BASE_URL = "https://www.semamart.com/"
-
-
-// test apis
-// export const API_URL = "https://test.semamart.com/api/v2/"
-// export const BASE_URL = "https://test.semamart.com/"
+export const API_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
