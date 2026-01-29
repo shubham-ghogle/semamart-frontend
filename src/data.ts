1149f73b83574a101d31a4cead82acebe4f80bba
apoorvjauhariinfo <apoorv.jauhari@innovyatech.com>
2026-01-29 13:12:03 +0530

    Test API

--- src/data.ts
+++ src/data.ts
@@ -3,5 +3,11 @@
 // export const BASE_URL = "http://localhost:8000/"
 
 
-export const API_URL = process.env.VITE_API_URL;
-export const BASE_URL = process.env.VITE_BASE_URL;
+// // // live apis
+// // export const API_URL = "https://www.semamart.com/api/v2/"
+// // export const BASE_URL = "https://www.semamart.com/"
+
+
+//test apis
+export const API_URL = "https://test.semamart.com/api/v2/"
+export const BASE_URL = "https://test.semamart.com/"
